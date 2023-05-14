1a5e7d2a69f81c49ebbc5b51910cfd1129338170
Daniel Schrader <98756494+DanSchrader@users.noreply.github.com>
2023-05-14 04:53:59 +0200

    another meta tryout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 'use client';
 
+import Head from 'next/head';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 export default function Home() {
     return (
-        <Box component='main'>
-            <Typography>
-                Home
-            </Typography>            
-        </Box>
+        <>
+            <Head>
+                <title>
+                    Reelizr
+                </title>
+                <meta 
+                    name='description'
+                    content='Reelizr: Deutschlands Top-Netzwerk-Hub für Brand Video Production. Vernetzen Sie sich mit Profis und steigern Sie Ihren Markenerfolg durch Business-Videos.'
+                    key='desc'
+                />
+            </Head>
+            <Box component='main'>
+                <Typography>
+                    Home
+                </Typography>            
+            </Box>
+        </>
     )
 };
