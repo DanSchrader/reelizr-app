ab89ad60d1fdc7eb6bc5c82e9e24bc9ffe78d42d
Daniel Schrader <98756494+DanSchrader@users.noreply.github.com>
2023-05-14 04:48:18 +0200

    more meta tryouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,13 @@
 import './globals.css';
 import defaultTheme from './theme/defaultTheme';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import Head from 'next/head';
 import NavBar from './components/NavBar';
 // import Footer from './components/Footer';
+
+/* export const metadata = {
+  title: 'Reelizr',
+  description: 'Reelizr: Deutschlands Top-Netzwerk-Hub für Brand Video Production. Vernetzen Sie sich mit Profis und steigern Sie Ihren Markenerfolg durch Business-Videos.',
+}; */
 
 export default function RootLayout({
   children,
@@ -14,9 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <Head>
-        <title>Reelizr</title>
-      </Head>
+      <title>
+        Reelizr
+      </title>
+      <meta 
+        name='description'
+        content='Reelizr: Deutschlands Top-Netzwerk-Hub für Brand Video Production. Vernetzen Sie sich mit Profis und steigern Sie Ihren Markenerfolg durch Business-Videos.'
+        key='desc'
+        />
       <body>
         <ThemeProvider theme={defaultTheme}>
           <CssBaseline />
