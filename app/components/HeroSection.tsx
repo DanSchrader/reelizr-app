6c19b20ecd52d209dc1afd5a6aaa4b20b53bfc4a
Daniel Schrader <98756494+DanSchrader@users.noreply.github.com>
2023-06-02 02:14:20 +0200

    add content to hero, make hero component, add Head to home
    at least locally Head is still buggy

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,83 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+export default function HeroSection() {
+    return (
+
+<Box component='section' sx={{
+                padding: {
+                    xs: '2rem',
+                    sm: '2rem 2rem 2rem 4rem',
+                    md: '2rem 2rem 2rem 4rem',
+                    lg: '2rem 8rem 2rem 12rem',
+                    xl: '2rem 12rem 2rem 16rem',
+                }
+            }}>
+                <Grid 
+                container 
+                spacing={2} 
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center', 
+                    alignItems: 'center',
+                    flexDirection: {
+                        xs: 'row', 
+                        sm: 'row-reverse', 
+                    },
+                }}>
+                    <Grid item xs={12} md={6} textAlign='center'>
+                        <Box 
+                        component='img' 
+                        src='./hero-logo.webp' 
+                        alt='Reelizr-Logo'
+                        sx={{
+                            height:'auto',
+                            width: {
+                                xs: '75%',
+                                sm: '50%',
+                                md: '75%',
+                            },
+                        }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Grid container spacing={2} sx={{
+                            textAlign: {
+                                xs: 'center',
+                                md: 'left',
+                            },
+                        }}>
+                            <Grid item xs={12}>
+                                <Typography component='h2' variant='h2' color='secondary'>
+                                        #REELIZR
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography component='h1' variant='h1'>
+                                        VIDEO MARKETING
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography sx={{ 
+                                    maxWidth: '80%', 
+                                    margin: {
+                                        xs: '0 auto',
+                                        md: '0',
+                                    },
+                                    }}>
+                                        Willkommen bei Reelizr, Ihrer Video Marketing Agentur. 
+                                        Wir verwandeln Ihre Vision in aufregendes Content Video Marketing. 
+                                        Nutzen Sie mit uns für Ihr Marketing Youtube - eine der größten Suchmaschinen der Welt.
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} mt={1}>
+                                <Button variant='contained'>Direkt loslegen</Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Box>
+    )
+};
--- app/page.tsx
+++ app/page.tsx
@@ -3,85 +3,36 @@
 import Head from 'next/head';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+
+import HeroSection from './components/HeroSection';
 
 export default function Home() {
     return (
-        <Box component='main'>
-            <Box component='section' sx={{
-                padding: {
-                    xs: '2rem',
-                    sm: '2rem 2rem 2rem 4rem',
-                    md: '2rem 2rem 2rem 4rem',
-                    lg: '2rem 8rem 2rem 12rem',
-                    xl: '2rem 12rem 2rem 16rem',
-                }
-            }}>
-                <Grid 
-                container 
-                spacing={2} 
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center', 
-                    alignItems: 'center',
-                    flexDirection: {
-                        xs: 'row', 
-                        sm: 'row-reverse', 
-                    },
-                }}>
-                    <Grid item xs={12} md={6} textAlign='center'>
-                        <Box 
-                        component='img' 
-                        src='./hero-logo.webp' 
-                        alt='Reelizr-Logo'
-                        sx={{
-                            height:'auto',
-                            width: {
-                                xs: '75%',
-                                sm: '50%',
-                                md: '75%',
-                            },
-                        }}
-                        />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Grid container spacing={2} sx={{
-                            textAlign: {
-                                xs: 'center',
-                                md: 'left',
-                            },
-                        }}>
-                            <Grid item xs={12}>
-                                <Typography component='h2' variant='h2' color='secondary'>
-                                        UNTERNEHMENSPRÄSENTATION
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography component='h1' variant='h1'>
-                                        VIDEO MARKETING
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography sx={{ 
-                                    maxWidth: '80%', 
-                                    margin: {
-                                        xs: '0 auto',
-                                        md: '0',
-                                    },
-                                    }}>
-                                Steigern Sie Ihren Brand-Value: <br /> 
-                                Mit maßgeschneiderten Brand-Videos, Drohnenaufnahmen und Video-Podcasts fördern wir Kundenbindung und Markenbekanntheit. <br /> 
-                                Entdecken Sie die Kraft der visuellen Kommunikation.
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12} mt={1}>
-                                <Button variant='contained'>Direkt loslegen</Button>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Box>    
-        </Box>
+        <>
+            <Head>
+                <title>Reelizr</title>
+                <meta 
+                    name='description'
+                    content='Deutschlands Top-Netzwerk-Hub für Brand Video Production. Vernetzen Sie sich mit Profis und steigern Sie Ihren Markenerfolg durch Business-Videos.'
+                    key='desc'
+                />
+                <meta 
+                    property='og:title' 
+                    content='Reelizr' />
+                <meta
+                    property='og:description'
+                    content='Deutschlands Top-Netzwerk-Hub für Brand Video Production. Vernetzen Sie sich mit Profis und steigern Sie Ihren Markenerfolg durch Business-Videos.'
+                />
+                <link rel='icon' href='/favicon.ico' sizes='any'/>
+            </Head>
+            <Box component='main'>
+                <HeroSection />
+                <Typography>
+                    Steigern Sie Ihren Brand-Value: <br /> 
+                    Mit maßgeschneiderten Brand-Videos, Drohnenaufnahmen und Video-Podcasts fördern wir Kundenbindung und Markenbekanntheit. <br /> 
+                    Entdecken Sie die Kraft der visuellen Kommunikation.
+                </Typography>    
+            </Box>
+        </>
     )
 };
